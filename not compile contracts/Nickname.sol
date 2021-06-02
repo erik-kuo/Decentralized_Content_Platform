32825b9a2ac3c9f52800bea3a642e60f14d2495f
t30t <71395346+t30t@users.noreply.github.com>
2021-06-02 21:51:57 +0800

    好耶，Nickname會動了

--- not compile contracts/Nickname.sol
+++ not compile contracts/Nickname.sol
@@ -1,11 +1,24 @@
-pragma solidity >0.5.0 <=0.6.0;
+pragma solidity >0.5.0 <0.6.0;
 
 contract Nickname {
     mapping(address => string) userNickname;
     
+    function _toString(bytes memory _data) private pure returns(string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        
+        bytes memory str = new bytes(2 + _data.length * 2);
+        str[0] = "0";
+        str[1] = "x";
+        for (uint i = 0; i < _data.length; i++) {
+            str[2+i*2] = alphabet[uint(uint8(_data[i] >> 4))];
+            str[3+i*2] = alphabet[uint(uint8(_data[i] & 0x0f))];
+        }
+        return string(str);
+    }
+    
     function getNickname(address _user) view external returns(string memory) {
-        if(userNickname[_user] == "") {
-            return abi.encodePacked(_user);
+        if(keccak256(abi.encodePacked(userNickname[_user])) == keccak256(abi.encodePacked(""))) {
+            return _toString(abi.encodePacked(_user));
         }
         else {
             return userNickname[_user];
