ed960f75d8b4871553161c5c1069ffc0aa70c893
t30t <71395346+t30t@users.noreply.github.com>
2021-06-17 22:26:05 +0800

    Added "category" attribute

--- contracts/PostManager.sol
+++ contracts/PostManager.sol
@@ -7,6 +7,7 @@ contract PostManager {
         address owner;
         string text;
         string[] imageHash;
+        uint category;
     }
     Post[] posts;
     
@@ -16,23 +17,24 @@ contract PostManager {
         return posts.length;
     }
     
-    function getPost(uint _id) view external returns(address owner, uint postTime, string memory content, string[] memory images) {
+    function getPost(uint _id) view external returns(address owner, uint postTime, string memory content, string[] memory images, uint category) {
         require(_id < posts.length);
         
         owner = posts[_id].owner;
         postTime = posts[_id].timestamp;
         content = posts[_id].text;
         images = posts[_id].imageHash;
+        category = posts[_id].category;
     }
     
-    function createPost(string memory _content, string[] memory _images) public {
-        uint id = posts.push(Post(now, msg.sender, _content, _images)) - 1;
+    function createPost(string memory _content, string[] memory _images, uint _category) public {
+        uint id = posts.push(Post(now, msg.sender, _content, _images, _category)) - 1;
         emit NewPost(msg.sender, id);
     }
     
     constructor() public {
         string[] memory images = new string[](1);
         images[0] = "QmZygsw7Z9TNYkmtqkjjtox5WU7pEMHwekAVGquC32vTCm";
-        createPost("Hello, world", images);
+        createPost("Hello, world", images, 0);
     }
 }
