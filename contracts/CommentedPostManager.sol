7ab87709dcc3988ba53b7038779b71a68d007e92
t30t <71395346+t30t@users.noreply.github.com>
2021-06-19 20:26:41 +0800

    有付款功能的comment，因為架構大改所以新增檔案

---
+++ contracts/CommentedPostManager.sol
@@ -0,0 +1,68 @@
+pragma solidity >0.5.0 <0.6.0;
+pragma experimental ABIEncoderV2;
+
+import "./PostManager.sol";
+
+contract CommentedPostManager is PostManager {
+    struct Comment {
+        uint postId;
+        address owner;
+        uint timestamp;
+        string text;
+        uint value;
+    }
+    Comment[] comments;
+    
+    uint commentCooldown = 5 seconds;
+    mapping(address => uint) nextCommentTime;
+    
+    event NewComment(uint indexed postId, address indexed owner, uint commentId);
+    
+    function getCommentCount() view external returns(uint) {
+        return comments.length;
+    }
+    
+    function getCommentCountByPost(uint _postId) view external returns(uint) {
+        uint result = 0;
+        for(uint id=0; id<comments.length; id++) {
+            if(comments[id].postId == _postId) {
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    function getComment(uint _id) view external returns(uint postId, address owner, uint postTime, string memory content, uint value) {
+        require(_id < comments.length);
+        
+        postId = comments[_id].postId;
+        owner = comments[_id].owner;
+        postTime = comments[_id].timestamp;
+        content = comments[_id].text;
+        value = comments[_id].value;
+    }
+    
+    function createComment(uint _postId, string calldata _content) external {
+        require(_postId < getPostCount());
+        require(nextCommentTime[msg.sender] <= now);
+        
+        uint id = comments.push(Comment(_postId, msg.sender, now, _content, 0)) - 1;
+        nextCommentTime[msg.sender] = now + commentCooldown;
+        
+        emit NewComment(_postId, msg.sender, id);
+    }
+    
+    function createPayingComment(uint _postId, string calldata _content) external payable {
+        require(msg.value > 0 wei);
+        require(_postId < getPostCount());
+        require(nextCommentTime[msg.sender] <= now);
+        
+        uint id = comments.push(Comment(_postId, msg.sender, now, _content, msg.value)) - 1;
+        nextCommentTime[msg.sender] = now + commentCooldown;
+        
+        address payable postOwner = _getPostOwner(_postId);
+        postOwner.transfer(msg.value);
+        
+        emit NewComment(_postId, msg.sender, id);
+    }
+}
